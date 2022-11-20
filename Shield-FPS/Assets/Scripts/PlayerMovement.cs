8e1e3e97abaa99c02436cd4b4625522f69dc7e17
Diego-59210 <diegomartinez59210@gmail.com>
2022-11-20 15:21:08 -0300

    Update PlayerMovement.cs

--- Shield-FPS/Assets/Scripts/PlayerMovement.cs
+++ Shield-FPS/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 20f;
+    public float momentumDamping = 5f;
     private CharacterController playerController;
 
     private Vector3 inputVector;
@@ -23,16 +24,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         PlayerInput();
-        MovePlayer();
-        CheckForWalk();
+        MovePlayer();  
 
         camAnim.SetBool("isWalking", isWalking);
     }
     void PlayerInput()
     {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        inputVector.Normalize();
-        inputVector = transform.TransformDirection(inputVector);
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            inputVector.Normalize();
+            inputVector = transform.TransformDirection(inputVector);
+
+            isWalking = true;
+        }
+        else
+        {
+            inputVector = Vector3.Lerp(inputVector, Vector3.zero, momentumDamping * Time.deltaTime);
+
+            isWalking = false;
+        }
 
         movementVector = (inputVector * playerSpeed) + (Vector3.up * gravity);
     }
@@ -40,17 +51,7 @@ public class PlayerMovement : MonoBehaviour
     {
         playerController.Move(movementVector * Time.deltaTime);
     }
-    void CheckForWalk()
-    {
-        if(playerController.velocity.magnitude > 0.1f)
-        {
-            isWalking = true;
-        }
-        else
-        {
-            isWalking = false;
-        }
-    }
+  
 
 
 }
