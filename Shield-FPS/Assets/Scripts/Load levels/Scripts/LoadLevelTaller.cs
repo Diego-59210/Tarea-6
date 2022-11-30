fdf843802267119b31d756601c07a98106487953
XoeZ <dubxten@gmail.com>
2022-11-30 15:05:24 -0300

    mas optimizacion :D

---
+++ Shield-FPS/Assets/Scripts/Load levels/Scripts/LoadLevelTaller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelTaller : MonoBehaviour
+{
+    [LevelSelector(false)]
+    public string levelToLoad;
+    
+    public void LoadLevelNowText()
+    {
+        Debug.Log("Load level [" + levelToLoad + "]");
+    }
+
+    public void LoadLevelNow()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void SetLevelToLoad(string newLevel)
+    {
+        levelToLoad = newLevel;
+    }
+}
+
+internal class LevelSelectorAttribute : Attribute
+{
+    private bool v;
+
+    public LevelSelectorAttribute(bool v)
+    {
+        this.v = v;
+    }
+}
