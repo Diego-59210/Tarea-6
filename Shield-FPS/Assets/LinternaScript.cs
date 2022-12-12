94b2f6d42b5f3fbe6b1ec5f3501544186c5f1b47
XoeZ <dubxten@gmail.com>
2022-12-11 21:06:53 -0300

    implementacion de escudo al nivel 1

---
+++ Shield-FPS/Assets/LinternaScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinternaScript : MonoBehaviour
+{
+    Animator anim;
+    Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        tr = transform;
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetBool("Linterna", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetBool("Linterna", true);
+        }
+    }
+
+}
